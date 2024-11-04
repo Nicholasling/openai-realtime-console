c8a2088950725aa9b42619c7ffd31d725b274456
blueberryling <nicholasing@gmail.com>
2024-11-04 13:43:32 +0000

    working

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ConsolePage } from './pages/ConsolePage';
+import AboutPage from './pages/AboutPage';
+import Navbar from './components/Navbar'; // Import the Navbar component
 import './App.scss';
 
 function App() {
   return (
-    <div data-component="App">
-      <ConsolePage />
-    </div>
+    <Router>
+      <div data-component="App">
+        <Navbar /> {/* Navbar is displayed on all pages */}
+        <Routes>
+          <Route path="/" element={<ConsolePage />} /> {/* Home page */}
+          <Route path="/about" element={<AboutPage />} /> {/* About page */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,16 @@
+// src/pages/AboutPage.tsx
+import React from 'react';
+// import './AboutPage.scss';
+import './ConsolePage.scss';
+
+
+const AboutPage: React.FC = () => {
+  return (
+    <div className="about-page">
+      <h1>About Us</h1>
+      <p>This is the About page of our application.</p>
+    </div>
+  );
+};
+
+export default AboutPage;
